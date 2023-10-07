0c0c7217bc63ec97cdd1f1f17348ce3fe1dc9d89
qlibane <qjirmo@gmail.com>
2023-10-07 09:24:15 -0700

    print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 97
+
+	;while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
