8580a9a72a612ea98474c882f3bc5b6627ac27e1
qlibane <qjirmo@gmail.com>
2023-10-10 07:33:20 -0700

    correction to 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,28 +11,26 @@ int main(void)
 
 	while (i <= '9')
 	{
-		putchar(i);
+		while (j <= '9')
+		{
+			if (!(i > j) || (i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
 		j = '0';
 		i++;
-	}
-	while (j <= '9')
-	{
-		putchar(j);
-		j++;
-	}
-	if (!(i > j) || (i == j))
-	{
-		putchar(i);
-		putchar(j);
-	}
-	if (i == 8 && j == 9)
-	{
-		putchar('\n');
-	}
-	else
-	{
-		putchar(',');
-		putchar(' ');
 	}
 	return (0);
 }
