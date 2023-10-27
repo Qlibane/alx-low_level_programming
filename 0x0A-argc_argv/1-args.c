95a926fc9efd80af0620985d8f4d002d6e2245ec
qlibane <qjirmo@gmail.com>
2023-10-27 12:47:36 -0700

    correction to 1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,9 @@
  *
  * @argv: pointer to an array of command line arguments
  *
- * Return (0)
+ * Return 0 success, non 0 fail
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
