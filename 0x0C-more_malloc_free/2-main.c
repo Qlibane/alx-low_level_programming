734acb3567929c262709448fc158723afb8524c5
qlibane <qjirmo@gmail.com>
2023-11-14 09:23:02 -0800

    function that prints a struct dog

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -1,35 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
+#include "dog.h"
 
 /**
  * main - check the code
@@ -38,13 +8,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char *a;
+    struct dog my_dog;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Best");
-    strcpy(a + 4, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    print_dog(&my_dog);
     return (0);
 }
